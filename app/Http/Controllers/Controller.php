5ffdba65a82709d017b97b58db7ecdfd05b7d607
ahmadfadillllah <ahmadfadillah502@gmail.com>
2024-10-30 16:46:22 +0800

    First

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
+use Kreait\Firebase\Factory;
 
 abstract class Controller
 {
     //
+    protected $apiFirebase;
+
+    public function __construct()
+    {
+        $serviceAccount = base_path('application-63b0a-firebase-adminsdk-zwnhv-0af0222886.json');
+
+        $factory = (new Factory)
+            ->withServiceAccount($serviceAccount)
+            ->withDatabaseUri('https://application-63b0a-default-rtdb.firebaseio.com/');
+
+        $this->apiFirebase = $factory->createDatabase();
+    }
+
+    public function getDataFireBase()
+        {
+            $snapshot = $this->apiFirebase->getReference()->getSnapshot();
+            $data = $snapshot->getValue();
+
+            return response()->json($data);
+        }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FirebaseController;
 use App\Http\Controllers\KKHController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('dashboards.index');
 });
+
+//firebase
+Route::get('/firebase/showData', [FirebaseController::class, 'showData']);
 
 //dashboard
 Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
